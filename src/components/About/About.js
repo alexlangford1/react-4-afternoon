04033050fab4c8307f98c663c4bfd9fb3a3bb41c
alex langford <alejandro010795@gmail.com>
2019-03-26 17:01:05 -0600

    completed routing

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,12 +1,51 @@
-import React, { Component } from 'react';
+import React, { Component } from "react"
+import { Link, Switch, Route } from "react-router-dom"
+import History from "../History/History"
+import Contact from "../Contact/Contact"
 
 export default class About extends Component {
   render() {
     return (
       <div>
-        <div className='subnav'>
+        <div className="subnav">
+          <Link to="/about" className="subnav_links">
+            <h3>About</h3>
+          </Link>
+          <Link to="/about/history" className="subnav_links">
+            <h3>History</h3>
+          </Link>
+          <Link to="/about/contact" className="subnav_links">
+            <h3>Contact</h3>
+          </Link>
         </div>
-        <div className='box'>
+
+        <div className="box">
+          <Switch>
+            <Route path="/about/history" component={History} />
+            <Route path="/about/contact" component={Contact} />
+            <Route
+              path="/about"
+              render={() => (
+                <div>
+                  <h1>About the University</h1>
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Totam et blanditiis unde aut natus dicta, libero veniam
+                    suscipit iusto iste omnis sit consequuntur voluptates, ipsum
+                    animi a quibusdam, fuga beatae. Lorem ipsum, dolor sit amet
+                    consectetur adipisicing elit. Laborum nemo deleniti,
+                    tempora, nobis dignissimos, similique ratione impedit
+                    tenetur ea officiis enim? Quos molestiae dignissimos sit
+                    ullam doloremque iste adipisci dolore. Lorem ipsum dolor sit
+                    amet consectetur adipisicing elit. Officia, ex aliquam?
+                    Pariatur reiciendis facilis nostrum quod doloremque ad
+                    architecto ipsum. Facere a labore tempore iure illo soluta
+                    rerum laboriosam ex.
+                  </p>
+                </div>
+              )}
+            />
+          </Switch>
         </div>
       </div>
     )
--- src/components/ClassList/ClassList.js
+++ src/components/ClassList/ClassList.js
@@ -1,17 +1,40 @@
-import React, { Component } from 'react';
+import React, { Component } from "react"
+import axios from "axios"
+import { Link } from "react-router-dom"
 
 export default class ClassList extends Component {
-  constructor() {
-    super()
-    
+  constructor(props) {
+    super(props)
+    this.state = {
+      students: [],
+    }
+  }
+
+  componentDidMount() {
+    axios
+      .get(
+        `http://localhost:3005/students?class=${this.props.match.params.class}`,
+      )
+      .then((res) => {
+        this.setState({ students: res.data })
+      })
+    console.log("1111111", this.props)
   }
 
   render() {
+    const student = this.state.students.map((e, i) => (
+      <Link to={`/student/${e.id}`} key={i}>
+        <h3>
+          {e.first_name} {e.last_name}
+        </h3>
+      </Link>
+    ))
     return (
       <div className="box">
-        <h1></h1>
-        <h2>ClassList:</h2>
-
+        <h1 style={{borderBottom: '3px solid black'}}>{this.props.match.params.class}</h1>
+        <h2 >ClassList:</h2>
+        {student}
+        <button><Link to='/' style={{color: 'white'}}>Return</Link></button>
       </div>
     )
   }
